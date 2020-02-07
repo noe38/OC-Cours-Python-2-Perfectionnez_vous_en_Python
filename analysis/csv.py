96ce26f31e05f1b0b462b7a869e81ec0b27d0559
noemie graignic <noemie.graignic@free.fr>
2020-02-07 22:43:58 +0100

    decouvrez les methodes speciales

--- analysis/csv.py
+++ analysis/csv.py
@@ -1,18 +1,21 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
 import os
 import pprint
 import logging as lg
 
-import numpy as np
 import pandas as pd
-#import matplotlib
-#matplotlib.use('TkAgg')
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns # rend plus joli Matplotlib
 
 
 #lg.basicConfig(level=lg.DEBUG)
 
-class SetOfParliamentMembers:
+class SetOfParliamentMember:
     def __init__(self, name):
         self.name = name
 
@@ -37,14 +40,14 @@ class SetOfParliamentMembers:
         fig, ax = plt.subplots()
         ax.axis("equal")
         ax.pie(
-            proportions,
-            labels=labels,
-            autopct="%1.1f pourcents"
-        )
+                proportions,
+                labels=labels,
+                autopct="%1.1f%%"
+                )
         plt.title("{} ({} MPs)".format(self.name, nb_mps))
         plt.show()
 
-    def split_by_political_part(self):
+    def split_by_political_party(self):
         result = {}
         data = self.dataframe
 
@@ -53,20 +56,114 @@ class SetOfParliamentMembers:
 
         for party in all_parties:
             data_subset = data[data.parti_ratt_financier == party]
-            subset = SetOfParliamentMembers('MPs from party "{}"'.format(party))
+            subset = SetOfParliamentMember('MPs from party "{}"'.format(party))
             subset.data_from_dataframe(data_subset)
             result[party] = subset
 
         return result
 
-def launch_analysis(data_file, by_party = False):
-    sopm = SetOfParliamentMembers("All MPs")
+    def __str__(self):
+        names = [] ## todo: remplacer a la fin par une comprehension
+        for row_index, mp in self.dataframe.iterrows(): ##todo: ici il y a du packing/unpacking
+            names += [mp.nom]
+        return str(names) # Python knows how to convert a list into a string
+
+    def __repr__(self):
+        return "SetOfParliamentMember: {} members".format(len(self.dataframe))
+
+    def __len__(self):
+        return self.number_of_mps
+
+    def __contains__(self, mp_name):
+        return mp_name in self.dataframe["nom"].values
+
+    def __getitem__(self, index):
+        try:
+            result = dict(self.dataframe.iloc[index])
+        except:
+            if index < 0:
+                raise Exception("Please select a positive index")
+            elif index >= len(self.dataframe):
+                raise Exception("There are only {} MPs!".format(len(self.dataframe)))
+            else:
+                raise Exception("Wrong index")
+        return result
+
+    def __add_(self, other):
+        if not isinstance(other, SetOfParliamentMember):
+            raise Exception("Can not add a SetOfParliamentMember with an object of type {}".format(type(other)))
+
+        df1, df2 = self.dataframe, other.dataframe ##todo: ici il y a du packing/unpacking
+        df = df1.append(df2)
+        df = df.drop_duplicates()
+
+        s = SetOfParliamentMember("{} - {}".format(self.name, other.name))
+        s.data_from_dataframe(df)
+        return s
+
+    def __radd__(self, other): ## todo : l'implementation de cette methode ne suit a mon avis pas les bonnes pratiques
+        return self
+
+    def __lt__(self, other):
+        return self.number_of_mps < other.number_of_mps
+
+    def __gt__(self, other):
+        return self.number_of_mps > other.number_of_mps
+
+    # The following 2 methods are a way to simulate a calculated attribute
+    # (attribute 'number_of_mps' is calculated from attribute 'self.dataframe')
+    # Tehre is a muche better way to do it, using decorator '@property'
+    def __getattr__(self, attr):
+        if attr == "number_of_mps": ##todo: faire la version avec #property
+            return len(self.dataframe)
+
+    def __setattr__(self, attr, value):
+        if attr == "number_of_mps":
+            raise Exception("You can not set the number of MPs!")
+        self.__dict__[attr] = value ## todo: c'est l'occasion de parler de __dict__ dans le cours
+
+
+def launch_analysis(data_file, by_party = False, info = False, displaynames = False, searchname = None, index = None, groupfirst = None):
+    sopm = SetOfParliamentMember("All MPs")
     sopm.data_from_csv(os.path.join("data", data_file))
     sopm.display_chart()
 
     if by_party:
         for party, s in sopm.split_by_political_party().items():
             s.display_chart()
+
+    if info:
+        print(sopm)
+        print(repr(sopm))
+
+    if displaynames:
+        print()
+        print(sopm)
+
+    if searchname != None:
+        is_present = searchname in sopm
+        print()
+        print("Testing if {} is present: {}".format(searchname, is_present))
+
+    if index is not None:
+        index = int(index)
+        print()
+        pprint.pprint(sopm[index]) # prints the dict a nice way
+
+    if groupfirst is not None:
+        groupfirst = int(groupfirst)
+        parties = sopm.split_by_political_party()
+        parties = parties.values()
+        parties_by_size = sorted(parties, reverse = True)
+
+        print()
+        print("Info: the {} biggest groups are :".format(groupfirst))
+        for p in parties_by_size[0:groupfirst]:
+            print(p.name)
+
+        s = sum(parties_by_size[0:groupfirst])
+
+        s.display_chart()
 
 
 """ def launch_analysis(data_file):
@@ -82,9 +179,5 @@ def launch_analysis(data_file, by_party = False):
  """
     
 
-def main():
-    launch_analysis('current_mps.csv')
-    
-
 if __name__ == "__main__":
-    main()
+    launch_analysis('current_mps.csv')
--- parite.py
+++ parite.py
@@ -6,15 +6,23 @@ import logging as lg
 import analysis.csv as c_an
 import analysis.xml as x_an
 
-lg.basicConfig(level=lg.DEBUG)
+log = lg.getLogger('Noemie_ici')
+log.setLevel(lg.DEBUG)
+#lg.basicConfig(level=lg.DEBUG)
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-e", "--extension", help="""Type of file to analyse. Is it a CSV or an XML""")
     parser.add_argument("-d", "--datafile", help="""CSV file containing pieces of information about the members of parliament""")
-    parser.add_argument("-v", "--verdose", action='store_true', help="""Make the application talk!""")
-    
+    #parser.add_argument("-v", "--verdose", action='store_true', help="""Make the application talk!""")
+    parser.add_argument("-i", "--info", action='store_true', help="""information about the file""")
+    parser.add_argument("-p", "--byparty", action='store_true', help="""displays a graph for each political party""")
+    parser.add_argument("-n", "--displaynames", action='store_true', help="""displays the names of all the mps""")
+    parser.add_argument("-s", "--searchname", help="""search for a mp name""")
+    parser.add_argument("-I", "--index", help="""displays information about th Ith mp""")
+    parser.add_argument("-g", "--groupfirst", help="""displays a graph groupping all the 'g' biggest political parties""")
+  
     return parser.parse_args()
 
 def main():
@@ -29,7 +37,7 @@ def main():
         lg.warning(e)
     else:
         if args.extension == 'csv':
-            c_an.launch_analysis(datafile)
+            c_an.launch_analysis(datafile, args.byparty, args.info, args.displaynames, args.searchname, args.index, args.groupfirst)
         elif args.extension == 'xml':
             x_an.launch_analysis(datafile)
     finally:
